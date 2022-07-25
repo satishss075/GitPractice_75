2b1841ea6658d30347ed1ebcd34bf5929377a555
satishss075 <satishss075@gmail.com>
2022-07-25 12:23:04 +0530

    first

--- src/test/java/monday/Pratice1.java
+++ src/test/java/monday/Pratice1.java
@@ -5,6 +5,8 @@ public class Pratice1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hiii");
+System.out.println("bye");
+System.out.println(" cccc");
 	}
 
 }
